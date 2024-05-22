5c0ab46d59bf8153fafc0b6c44489cf4c1d33246
aryan kaul <aryankaul2001@gmail.com>
2024-05-22 16:27:58 +0530

    section 3 start

--- section-03-the-basics/recipe-book-app/src/app/app.component.spec.ts
+++ section-03-the-basics/recipe-book-app/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-app'`, () => {
+  it(`should have as title 'recipe-book-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-app');
+    expect(app.title).toEqual('recipe-book-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-first-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, recipe-book-app');
   });
 });
