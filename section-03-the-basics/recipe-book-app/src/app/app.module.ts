8c18573738dc1080176b7f7b81b71d8b33216537
aryan kaul <aryankaul2001@gmail.com>
2024-05-24 19:18:53 +0530

    header added

--- section-03-the-basics/recipe-book-app/src/app/app.module.ts
+++ section-03-the-basics/recipe-book-app/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipeBookComponent } from './components/recipe-book/recipe-book.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
